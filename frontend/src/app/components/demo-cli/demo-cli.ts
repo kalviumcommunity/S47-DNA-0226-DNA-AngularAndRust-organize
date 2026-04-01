5c2a6be06b0ef1435b0ace69b30b978a7cca92ea
Akshit-101 <akshit.p@kalvium.community>
2026-04-01 11:52:50 +0530

    Task 7: Demonstrate Angular CLI commands (generate, serve, build)

---
+++ frontend/src/app/components/demo-cli/demo-cli.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-demo-cli',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './demo-cli.html',
+  styleUrl: './demo-cli.css',
+})
+export class DemoCliComponent {}
